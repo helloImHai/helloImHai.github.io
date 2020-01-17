a498acfd539fb60cc13a6720c61d2d87c64e5e8b
helloImHai <1503.hai.nguyen@gmail.com>
2020-01-17 12:15:55 +0800

    Reorganise files

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Hello Everybody!
+      </header>
+    </div>
+  );
+}
+
+export default App;
