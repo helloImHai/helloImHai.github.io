3a14b37e9084d09090ce2b0bdbac0aa417031b4c
helloImHai <1503.hai.nguyen@gmail.com>
2020-01-18 18:59:28 +0800

    Fix about page typos

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,12 +12,11 @@ class About extends Component {
           <h2>About Me</h2>
           <img className="profile-pic" src={profilePic} alt="profile-pic" />
           <p className="description">
-            Hello, my name is Hai, a Year 2 <strong>Computer Science</strong> student in the <strong>National University
-            of Singapore</strong>. I love what I am studying and I am still in the midst of exploring all the possibilites
-            that comes with diving into the field of Computing. Most of my experience lie in Web development and
-            Software Engineering, but I'm also enthusiastic about Algorithms, Artificial Inteligence and Information Security.
-            Right now, I'm looking for opportunities that will broaden my skillset and knowledge,
-            while at the same time allow me to contribute and leave a positive impact.
+            Hello, my name is Hai, a Year 2 <strong>Computer Science</strong> student at the <strong>National University of Singapore</strong>.
+            I love what I am studying and I am still in the midst of exploring all the possibilities that comes with diving 
+            into the field of Computing. Most of my experience lies in Web development and Software Engineering, but I'm also 
+            enthusiastic about Algorithms, Artificial Intelligence and Information Security. Right now, I'm looking for opportunities 
+            that will broaden my skills and knowledge, while at the same time allow me to contribute and leave a positive impact.
             <br /><br />
             When I am not coding I like kayaking, eating, and gymming. A weird hobby of mine is translating English songs
             into different languages.
@@ -26,16 +25,16 @@ class About extends Component {
         </Container>
         <div>
           <div className="hobby-item">
-          <FontAwesomeIcon className="hobby-icon" icon={faSearch}></FontAwesomeIcon>
-          <h5>Learn</h5>
+            <FontAwesomeIcon className="hobby-icon" icon={faSearch}></FontAwesomeIcon>
+            <h5>Learn</h5>
           </div>
           <div className="hobby-item">
-          <FontAwesomeIcon className="hobby-icon" icon={faHammer}></FontAwesomeIcon>
-          <h5>Build</h5>
+            <FontAwesomeIcon className="hobby-icon" icon={faHammer}></FontAwesomeIcon>
+            <h5>Build</h5>
           </div>
           <div className="hobby-item">
-          <FontAwesomeIcon className="hobby-icon" icon={faDumbbell}></FontAwesomeIcon>
-          <h5>Gym</h5>
+            <FontAwesomeIcon className="hobby-icon" icon={faDumbbell}></FontAwesomeIcon>
+            <h5>Gym</h5>
           </div>
         </div>
         <FontAwesomeIcon className="arrow-down" icon={faArrowCircleDown} />
