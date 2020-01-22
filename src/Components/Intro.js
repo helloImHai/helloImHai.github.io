530dbba01e0553de80d06232ed34e31b428928d7
helloImHai <1503.hai.nguyen@gmail.com>
2020-01-22 12:25:36 +0800

    Stop playing

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
 import { Container } from "@material-ui/core";
 import ScrollButton from "./ScrollButton.js";
+import { Fade } from "react-awesome-reveal";
 
 class Intro extends Component {
   render() {
     return (
       <Container className="container-1" maxWidth="xl">
-        <Container className="container-inner" maxWidth="md">
-          <h1 className="greeting">
-            HELLO <strong className="friend-name">FRIEND</strong>,<br />
-            I'M <strong className="my-name">HAI</strong>!
-          </h1>
-          <br />
-          <h2 className="short-line">
-            I like to <strong className="desc-word">build things</strong>
-            {" and "}<strong className="desc-word">solve problems</strong>.
-          </h2>
-        </Container>
-        <ScrollButton to="aboutSection" />
+        <Fade big duration={3000} triggerOnce={true}>
+          <Container className="container-inner" maxWidth="md">
+            <h1 className="greeting">
+              HELLO <strong className="friend-name">FRIEND</strong>,<br />
+              I'M <strong className="my-name">HAI</strong>!
+            </h1>
+            <br />
+            <h2 className="short-line">
+              I like to <strong className="desc-word">build things</strong>
+              {" and "}
+              <strong className="desc-word">solve problems</strong>.
+            </h2>
+            <ScrollButton to="aboutSection" />
+          </Container> 
+        </Fade>
       </Container>
     );
   }
