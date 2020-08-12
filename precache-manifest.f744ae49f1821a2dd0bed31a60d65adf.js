fa9580d45ac659b3ccb44a65be46f873acab0b50
helloimhai <hai.nguyen@ninjavan.co>
2020-08-13 02:27:23 +0800

    Updates

---
+++ precache-manifest.f744ae49f1821a2dd0bed31a60d65adf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48698abbb1a2a7c10a18fa8a5c96061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2fa15e40d23b3a8a1b3",
+    "url": "/static/css/main.c1c3b7bf.chunk.css"
+  },
+  {
+    "revision": "b00b2c02134d2fb42b2f",
+    "url": "/static/js/2.9b25f45e.chunk.js"
+  },
+  {
+    "revision": "1aff40f6cd2589c1f06af7e73a2046f9",
+    "url": "/static/js/2.9b25f45e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2fa15e40d23b3a8a1b3",
+    "url": "/static/js/main.4e55feba.chunk.js"
+  },
+  {
+    "revision": "54c4f745e1b15d3186ec",
+    "url": "/static/js/runtime-main.7a4d8891.js"
+  },
+  {
+    "revision": "755ab6b605b0cf95fd17d8bec1617536",
+    "url": "/static/media/ama.755ab6b6.png"
+  },
+  {
+    "revision": "f925f1166c07e1bd9f3cf6fcf557b419",
+    "url": "/static/media/cirbd.f925f116.png"
+  },
+  {
+    "revision": "ce80b1e4eae8a2e81e9b6b0c60cc7389",
+    "url": "/static/media/jokes.ce80b1e4.png"
+  },
+  {
+    "revision": "8b65198f0ac5298c9b73c07eb67d59b9",
+    "url": "/static/media/musicMaster.8b65198f.png"
+  },
+  {
+    "revision": "dc6a8249e0ee997900ca5f98ad0b9f90",
+    "url": "/static/media/portfolioV1.dc6a8249.png"
+  },
+  {
+    "revision": "093a76aa5b13e363ec46185d4b8fb885",
+    "url": "/static/media/portfolioV2.093a76aa.png"
+  },
+  {
+    "revision": "89b805fc5468ef9ee82a728b0523e7c2",
+    "url": "/static/media/profile.89b805fc.png"
+  },
+  {
+    "revision": "be2f4d9eb06847b4ba9c8b74ceaf76b3",
+    "url": "/static/media/sourceAcademy.be2f4d9e.png"
+  }
+]);
