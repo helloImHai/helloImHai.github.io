273c8c7a0c0be291e1dbd64048a20dbd786dda8b
helloimhai <hai.nguyen@ninjavan.co>
2020-08-13 02:16:56 +0800

    Updates

--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import navbarReducer from './navbarReducer';
-
-export default combineReducers({navbar: navbarReducer});
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import rootReducer from './reducers';
-import { createStore } from 'redux';
-
-const initialState = {};
-
-const store = createStore(rootReducer, initialState, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-export default store;
