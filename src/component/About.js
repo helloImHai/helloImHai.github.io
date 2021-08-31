8308e762c5c4045ac291de71e717fea5a46ef3a0
Hai Nguyen <50471593+helloImHai@users.noreply.github.com>
2021-08-31 08:57:04 +0800

    fix resume and transcript

--- src/component/About.js
+++ src/component/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Button, Box, Grid, Typography } from "@material-ui/core";
 import profilePic from "../Utils/profile.png";
 import HContainer from "./HContainer"
 import { makeStyles } from "@material-ui/core/styles";
@@ -14,7 +14,6 @@ const useStyle = makeStyles({
         boxShadow: "0px 0px 0px 5px rgba(255, 255, 255, 0.8)",
     }
 });
-
 
 function About() {
     const classes = useStyle();
@@ -41,6 +40,22 @@ function About() {
                         I have experience working as a backend web engineer in large scale payments 
                         and trading platforms... I'm a quick learner and always open to tackle new challenges.
                     </Typography>
+                </Grid>
+                <Box m={2}/>
+                <Grid container direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={3}>
+                    <Grid container item xs={6} justifyContent="flex-end">
+                        <Button variant="outlined" color="inherit" href="/resume.pdf">
+                            Grab my resume
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Button variant="outlined" color="inherit" href="/transcript.pdf">
+                            Peek my transcript
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
             <Box m={10}/>
